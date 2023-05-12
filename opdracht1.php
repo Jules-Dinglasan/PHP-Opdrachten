2ed01a9d611c497f36a9119fdcc01289211493d5
Jules-Dinglasan <133200504+Jules-Dinglasan@users.noreply.github.com>
2023-05-12 13:42:04 +0200

    Add files via upload

---
+++ opdracht1.php
@@ -0,0 +1,20 @@
+<?php
+$var1 = 12;
+$var2 = 10;
+$sum = var1 + var2;
+echo $sum . PHP_EOL;
+
+$var1 = 12;
+$var2 = 10;
+$product = var1 * var2;
+echo $product . PHP_EOL;
+
+ 
+$name = "Jules";
+echo "Welkom" . $name . "!";
+
+
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"; . PHP_EOL;
+?>
